f1c8eda7295cde0eb7b936c1371072a992a34b6f
omygan <1678212947@qq.com>
2022-11-21 14:50:48 +0800

    Site updated: 2022-11-21 14:50:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3fcfc58a.html","posts/d95d7e09.html","posts/e1962cae.html","posts/8892ccdc.html","posts/9c989aa7.html","posts/f8e09374.html","posts/d0a69f1e.html","posts/d8a440d1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/d95d7e09.html","posts/d0a69f1e.html","posts/3fcfc58a.html","posts/d8a440d1.html","posts/f8e09374.html","posts/e1962cae.html","posts/8892ccdc.html","posts/9c989aa7.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
